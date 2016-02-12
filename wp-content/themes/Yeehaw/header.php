4153fb5f7834ccc63f695f07316c962f423bb340
markchern <chern.mark@yahoo.com>
2016-02-12 13:16:58 -0800

    html5 comments

--- wp-content/themes/Yeehaw/header.php
+++ wp-content/themes/Yeehaw/header.php
@@ -39,6 +39,8 @@
 		</a>
 	</div>
 	<?php endif; ?>
+
+	<?php // THIS IS HTML5 ?>
 	<header id="masthead" class="site-header" role="banner">
 		<div class="header-main">
 			<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
