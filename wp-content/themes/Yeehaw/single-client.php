8c1bff24a4ebfc5f219c0b264db6e4500cb448cb
markchern <chern.mark@yahoo.com>
2016-02-12 11:22:42 -0800

    css edit and new template

--- wp-content/themes/Yeehaw/single-client.php
+++ wp-content/themes/Yeehaw/single-client.php
@@ -9,7 +9,7 @@
 
 get_header(); ?>
 
-	<div id="primary" class="content-area" style="background-color: pink">
+	<div id="primary" class="content-area pink">
 		<div id="content" class="site-content" role="main">
 			<?php
 				// Start the Loop.
